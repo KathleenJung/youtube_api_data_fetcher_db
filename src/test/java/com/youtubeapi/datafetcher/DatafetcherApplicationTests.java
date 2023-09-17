20ef2bda655cfc379e30395760d6eea3ce25284e
Kathleen Jung <ovo0325@outlook.kr>
2023-09-18 08:51:40 +0900

    Initial commit

---
+++ src/test/java/com/youtubeapi/datafetcher/DatafetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubeapi.datafetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatafetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
